edb5866a2cb4da8ebab0f8aa2fb3bed307899ef2
Varshini <varshinikarthik002@gmail.com>
2023-01-23 20:41:50 +0530

    day 2 changes

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
 'use strict';
-document.querySelector('.message').textContent
 
+let score = 20;
+let highscore = 0;
+let secretNum =Math.trunc(Math.random()*20)+1;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess=Number(document.querySelector('.guess').value)
+
+    if(!guess){
+        document.querySelector('.message').textContent='❌ No Number';
+    }
+    else if(guess === secretNum){
+            document.querySelector('.message').textContent ='🎉 Correct Number';
+            document.querySelector('.number').textContent = secretNum;
+            document.querySelector('body').style.backgroundColor='#60b347';
+            document.querySelector('.number').style.width='30rem';
+            if(score>highscore){
+                highscore=score;
+                document.querySelector('.highscore').textContent=highscore;
+            }
+        }
+    else if(guess > secretNum){
+        if(score >1){
+            document.querySelector('.message').textContent ='📈 Too high'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent ='👎🏼You lost the game'
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    }
+    else if(guess < secretNum){
+        if(score >1){
+            document.querySelector('.message').textContent ='📉 Too low'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent ='👎🏼You lost the game'
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    }
+})
+document.querySelector('.again').addEventListener('click',function(){
+    score=20;
+    secretNum =Math.trunc(Math.random()*20)+1;
+    document.querySelector('.message').textContent ='Start guessing'
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value='';
+    document.querySelector('body').style.backgroundColor='#222';
+    document.querySelector('.number').style.width='15rem';
+})
